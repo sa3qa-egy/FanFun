a62b31b84ffbfa1d4cf58820f88f32960224360a
yassen <yassenyasooo50@gmail.com>
2026-05-21 00:51:16 +0300

    Initial Commit

---
+++ FanFun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FanFun
+//
+//  Created by yassen on 21/05/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
