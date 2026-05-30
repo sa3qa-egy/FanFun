3b260d20e59454aae40126dde2eaf2210eec21d8
yassen <yassenyasooo50@gmail.com>
2026-05-30 04:15:28 +0300

    feat(leagues): implement league list view with search   filtering and loading states

--- FanFun/Presentation/Leagues/LeagueViewController.swift
+++ FanFun/Presentation/Leagues/LeagueViewController.swift
@@ -9,21 +9,97 @@ import UIKit
 
 class LeagueViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet weak var searchBar: UISearchBar!
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var tableView: UITableView!
+    var presenter: LeaguePresenterProtocol!
+        
+        private let activityIndicator = UIActivityIndicatorView(style: .large)
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            title = "leagues"
+            setupUI()
+            presenter.viewDidLoad()
+        }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
     }
-    */
+        
+        private func setupUI() {
+            
+            title = "Leagues"
+                
+                tableView.dataSource = self
+                tableView.delegate = self
+                searchBar.delegate = self
+            tableView.register(LeagueTableViewCell.self, forCellReuseIdentifier: "LeagueCell")
+
+            tableView.rowHeight = UITableView.automaticDimension
+            tableView.estimatedRowHeight = 90
+                tableView.rowHeight = UITableView.automaticDimension
+                tableView.estimatedRowHeight = 90
+            
+            activityIndicator.center = view.center
+            activityIndicator.hidesWhenStopped = true
+            view.addSubview(activityIndicator)
+            
+            tableView.keyboardDismissMode = .onDrag
+        }
+    }
+
+    extension LeagueViewController: LeagueViewProtocol {
+        func showLoading() {
+            DispatchQueue.main.async { self.activityIndicator.startAnimating() }
+        }
+        
+        func hideLoading() {
+            DispatchQueue.main.async { self.activityIndicator.stopAnimating() }
+        }
+        
+        func reloadTableView() {
+            DispatchQueue.main.async { self.tableView.reloadData() }
+        }
+        
+        func showError(message: String) {
+            DispatchQueue.main.async {
+                let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default))
+                self.present(alert, animated: true)
+            }
+        }
+    }
+
+    extension LeagueViewController: UITableViewDataSource, UITableViewDelegate {
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return presenter.numberOfLeagues
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "LeagueCell", for: indexPath) as? LeagueTableViewCell else {
+                fatalError("Could not dequeue LeagueTableViewCell. Check Storyboard identifier.")
+            }
+            
+            let league = presenter.getLeague(at: indexPath.row)
+            cell.configure(with: league)
+            
+            return cell
+        }
+        
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+    }
+
+    extension LeagueViewController: UISearchBarDelegate {
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            presenter.filterLeagues(with: searchText)
+        }
+        
+        func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+            searchBar.resignFirstResponder()
+        }
 
 }
--- FanFun/Presentation/Leagues/LeaguesPresenter.swift
+++ FanFun/Presentation/Leagues/LeaguesPresenter.swift
@@ -1,35 +1,53 @@
 import Foundation
 
-protocol LeaguesViewProtocol: AnyObject {
-    func showLoading()
-    func hideLoading()
-    func displayLeagues(jsonString: String)
-    func showError(message: String)
-}
-
-class LeaguesPresenter {
-    private weak var view: LeaguesViewProtocol?
+class LeaguePresenter: LeaguePresenterProtocol {
+    private weak var view: LeagueViewProtocol?
     private let repository: LeagueRepositoryProtocol
+    private let sportType: String
     
-    init(view: LeaguesViewProtocol, repository: LeagueRepositoryProtocol = LeagueRepository()) {
+    private var allLeagues: [League] = []
+    private var filteredLeagues: [League] = []
+    
+    init(view: LeagueViewProtocol, repository: LeagueRepositoryProtocol = LeagueRepository(), sportType: String) {
         self.view = view
         self.repository = repository
+        self.sportType = sportType
+    }
+    
+    var numberOfLeagues: Int {
+        return filteredLeagues.count
     }
     
     func viewDidLoad() {
+        fetchLeagues()
+    }
+    
+    func getLeague(at index: Int) -> League {
+        return filteredLeagues[index]
+    }
+    
+    func filterLeagues(with query: String) {
+        if query.trimmingCharacters(in: .whitespaces).isEmpty {
+            filteredLeagues = allLeagues
+        } else {
+            filteredLeagues = allLeagues.filter { $0.leagueName.localizedCaseInsensitiveContains(query) }
+        }
+        view?.reloadTableView()
+    }
+    
+    private func fetchLeagues() {
         view?.showLoading()
-        repository.getLeagues { [weak self] result in
-            self?.view?.hideLoading()
+        repository.getLeagues(for: sportType) { [weak self] result in
+            guard let self = self else { return }
+            self.view?.hideLoading()
+            
             switch result {
             case .success(let leagues):
-                let encoder = JSONEncoder()
-                encoder.outputFormatting = .prettyPrinted
-                if let data = try? encoder.encode(leagues),
-                   let jsonString = String(data: data, encoding: .utf8) {
-                    self?.view?.displayLeagues(jsonString: jsonString)
-                }
+                self.allLeagues = leagues
+                self.filteredLeagues = leagues
+                self.view?.reloadTableView()
             case .failure(let error):
-                self?.view?.showError(message: error.localizedDescription)
+                self.view?.showError(message: error.localizedDescription)
             }
         }
     }
