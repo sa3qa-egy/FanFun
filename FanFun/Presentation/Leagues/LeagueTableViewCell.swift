62572f7796c6d30a82a0a58449b7dc26393c8f5c
yassen <yassenyasooo50@gmail.com>
2026-05-30 04:13:41 +0300

    update network service and repository to fetch   leagues by sport type

---
+++ FanFun/Presentation/Leagues/LeagueTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LeagueTableViewCell.swift
+//  FanFun
+//
+//  Created by yassen on 30/05/2026.
+//
+
+import UIKit
+
+class LeagueTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
