ee36347239f6ead470e07b54b9a26627ae6a218d
Abdelhamed-Zyada <ab.zy.az30@gmail.com>
2026-06-02 04:03:06 +0300

    feat: build league details UI with storyboard and XIB cells

---
+++ FanFun/Presentation/LeagueDetails/TeamCell.swift
@@ -0,0 +1,44 @@
+//
+//  TeamCell.swift
+//  FanFun
+//
+
+import UIKit
+import SDWebImage
+
+class TeamCell: UICollectionViewCell {
+    
+    static let reuseIdentifier = "TeamCell"
+    
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var teamImageView: UIImageView!
+    @IBOutlet weak var teamNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = .clear
+        
+        containerView.layer.shadowColor = UIColor.black.cgColor
+        containerView.layer.shadowOpacity = 0.2
+        containerView.layer.shadowOffset = CGSize(width: 0, height: 2)
+        containerView.layer.shadowRadius = 4
+        containerView.clipsToBounds = false
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        teamImageView.layer.cornerRadius = teamImageView.frame.height / 2
+    }
+    
+    func configure(with team: Team) {
+        teamNameLabel.text = team.teamName
+        
+        let placeholder = UIImage(named: "leauge_placeholder")
+        
+        if let urlString = team.teamLogo, let url = URL(string: urlString) {
+            teamImageView.sd_setImage(with: url, placeholderImage: placeholder)
+        } else {
+            teamImageView.image = placeholder
+        }
+    }
+}
---
+++ FanFun/Presentation/LeagueDetails/UpcomingMatchCell.swift
@@ -0,0 +1,61 @@
+//
+//  UpcomingMatchCell.swift
+//  FanFun
+//
+
+import UIKit
+import SDWebImage
+
+class UpcomingMatchCell: UICollectionViewCell {
+    
+    static let reuseIdentifier = "UpcomingMatchCell"
+    
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var matchNameLabel: UILabel!
+    @IBOutlet weak var homeTeamImageView: UIImageView!
+    @IBOutlet weak var homeTeamNameLabel: UILabel!
+    @IBOutlet weak var awayTeamImageView: UIImageView!
+    @IBOutlet weak var awayTeamNameLabel: UILabel!
+    @IBOutlet weak var dateTimeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = .clear
+        
+        containerView.layer.shadowColor = UIColor.black.cgColor
+        containerView.layer.shadowOpacity = 0.3
+        containerView.layer.shadowOffset = CGSize(width: 0, height: 4)
+        containerView.layer.shadowRadius = 8
+        containerView.clipsToBounds = false
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        homeTeamImageView.layer.cornerRadius = homeTeamImageView.frame.height / 2
+        awayTeamImageView.layer.cornerRadius = awayTeamImageView.frame.height / 2
+    }
+    
+    func configure(with fixture: Fixture) {
+        let roundText = fixture.leagueRound ?? "Match"
+        matchNameLabel.text = roundText.isEmpty ? "Match" : roundText
+        
+        homeTeamNameLabel.text = fixture.eventHomeTeam
+        awayTeamNameLabel.text = fixture.eventAwayTeam
+        
+        dateTimeLabel.text = "\(fixture.eventDate)\n\(fixture.eventTime)"
+        
+        let placeholder = UIImage(named: "leauge_placeholder")
+        
+        if let urlString = fixture.homeTeamLogo, let url = URL(string: urlString) {
+            homeTeamImageView.sd_setImage(with: url, placeholderImage: placeholder)
+        } else {
+            homeTeamImageView.image = placeholder
+        }
+        
+        if let urlString = fixture.awayTeamLogo, let url = URL(string: urlString) {
+            awayTeamImageView.sd_setImage(with: url, placeholderImage: placeholder)
+        } else {
+            awayTeamImageView.image = placeholder
+        }
+    }
+}
