92ff5a957d6a7e63cec306b464da1615003c31d4
yassen <yassenyasooo50@gmail.com>
2026-06-03 19:04:26 +0300

    feat:add team details view controller and build the ui in story board

---
+++ FanFun/Presentation/TeamDetails/TeamDetailsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TeamDetailsViewController.swift
+//  FanFun
+//
+//  Created by yassen on 03/06/2026.
+//
+
+import UIKit
+
+class TeamDetailsViewController: UIViewController {
+    @IBOutlet weak var teamImage: UIImageView!
+    
+    @IBOutlet weak var tabelView: UITableView!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
