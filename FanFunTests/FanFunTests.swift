f11195fb57f3ce488501e3b2e05a963adc17b6a6
yassen <yassenyasooo50@gmail.com>
2026-06-06 00:59:32 +0300

    add base FanFunTests target initialization file

---
+++ FanFunTests/FanFunTests.swift
@@ -0,0 +1,15 @@
+//
+//  FanFunTests.swift
+//  FanFunTests
+//
+//  Created by yassen on 06/06/2026.
+//
+
+import Testing
+
+struct FanFunTests {
+
+    @Test func example() async throws {
+    }
+
+}
