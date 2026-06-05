bcf9af3c874dd4ca29d91fa786d64ae6e4181776
yassen <yassenyasooo50@gmail.com>
2026-06-05 20:40:15 +0300

    redesign sport cell withsurface card color  and overflow image
    - Applied custom ff_surface and ff_background colors.

--- FanFun/Presentation/Home/SportCollectionViewCell.swift
+++ FanFun/Presentation/Home/SportCollectionViewCell.swift
@@ -9,32 +9,64 @@ import UIKit
 
 class SportCellCollectionViewCell: UICollectionViewCell {
 
-    
+
     @IBOutlet weak var containerView: UIView!
-    
     @IBOutlet weak var sportName: UILabel!
     @IBOutlet weak var SportImage: UIImageView!
+
+    private var cardView: UIView? { containerView.subviews.first }
+
+    private let accentBorderLayer = CALayer()
+
     override func awakeFromNib() {
-            super.awakeFromNib()
-            setupUI()
+        super.awakeFromNib()
+        setupUI()
+    }
+
+    private func setupUI() {
+        backgroundColor = .clear
+        containerView.backgroundColor = .clear
+        containerView.clipsToBounds = false
+
+        guard let card = cardView else { return }
+
+        card.backgroundColor = UIColor(named: "ff_surfuce")
+        card.layer.cornerRadius = 16
+        card.clipsToBounds = false
+        card.layer.shadowColor   = UIColor.black.cgColor
+        card.layer.shadowOpacity = 0.28
+        card.layer.shadowRadius  = 10
+        card.layer.shadowOffset  = CGSize(width: 0, height: 4)
+
+        let primary = UIColor(named: "ff_primary")
+            ?? UIColor(red: 0.286, green: 0.725, blue: 0.192, alpha: 1)
+        accentBorderLayer.borderColor  = primary.withAlphaComponent(0.30).cgColor
+        accentBorderLayer.borderWidth  = 1
+        accentBorderLayer.cornerRadius = 16
+        card.layer.addSublayer(accentBorderLayer)
+
+        sportName.textColor = UIColor(named: "ff_primary_text")
+        sportName.font      = UIFont.systemFont(ofSize: 15, weight: .semibold)
+
+        SportImage.contentMode = .scaleAspectFit
+        if let imgSuperview = SportImage.superview {
+            imgSuperview.bringSubviewToFront(SportImage)
         }
-        
-        private func setupUI() {
-            containerView.layer.cornerRadius = 16
-            containerView.clipsToBounds = true
-            containerView.backgroundColor = .clear
-            
-            let blurEffect = UIBlurEffect(style: .systemUltraThinMaterial)
-            let blurView = UIVisualEffectView(effect: blurEffect)
-            
-            blurView.frame = containerView.bounds
-            blurView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-            
-            containerView.insertSubview(blurView, at: 0)
-        }
-        
-        func configure(with sport: Sport) {
-            sportName.text = sport.name
-            SportImage.image = sport.image
-        }
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        guard let card = cardView else { return }
+        accentBorderLayer.frame = card.bounds
+
+        card.layer.shadowPath = UIBezierPath(
+            roundedRect: card.bounds, cornerRadius: 16
+        ).cgPath
+    }
+
+
+    func configure(with sport: Sport) {
+        sportName.text   = sport.name
+        SportImage.image = sport.image
+    }
 }
